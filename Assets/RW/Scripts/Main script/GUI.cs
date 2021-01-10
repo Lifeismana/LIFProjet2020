68b465472d235e4b61c2f724012f3d7215a72b72
Jonathan <Sir.Lorrak@gmail.com>
2021-01-10 19:16:19 +0100

    meilleur interface

--- Assets/RW/Scripts/Main script/GUI.cs
+++ Assets/RW/Scripts/Main script/GUI.cs
@@ -14,63 +14,96 @@ public class GUI : MonoBehaviour
 	private bool pres,feu;
 	public float compteur = 3;
 	private float chronometre = 0.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-    	  loading.text="Chargement.";
-        countdown.text = "";
-        chrono.text = "";
 
-        pres = false;
-        feu = false;
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		loading.text="Chargement.";
+		countdown.text = "";
+		chrono.text = "";
 
-    // Update is called once per frame
-    void Update()
-    {
-    	int t = (int)Time.time;
-    	if(feu){
-    		string separateur = ":";
-    		chronometre+=Time.deltaTime;
-    		int unite = (int)chronometre;
-    		int decim = (int)((chronometre-unite)*100.0);
-    		if(decim<10){
-    			separateur+="0";
-    		}
-    		chrono.text = unite.ToString()+separateur+decim.ToString();
-    	}
-    	if(pres){
-	    	if(!GameObject.Find("4_RWrist")){
-	    		loading.text="placer vous bien devant la caméra";
-	    	}
-	    	if(compteur<=0 && !feu){
+		pres = false;
+		feu = false;
+	}
+
+	void updateLoading(){
+		int t = (int)Time.time;
+		if(!pres){
+			//"animation" du chargement
+			if(t%3==0)
+				loading.text="Chargement.";
+
+			if(t%3==1)
+				loading.text="Chargement..";
+
+			if(t%3==2)
+				loading.text="Chargement...";
+
+			if(FPS.text != "0.0 FPS"){
+				//début du compte a rebourd
+				pres = true;
+				loading.text = "";
+			}
+		}
+		else{
+			if(!GameObject.Find("4_RWrist")){
+				loading.text="placer vous bien en face de votre caméra";
+				loading.fontSize = 100;
+			}
+			else
+				loading.text="";
+		}
+			
+	}
+
+	void updateCountdown(){
+		if(pres){
+	    	compteur-=Time.deltaTime;
+	    	countdown.text = ((int)compteur+1).ToString();
+
+	    	if(compteur <= 0){
 	    		feu = true;
-	    		loading.text = "ramez !";
+	    		countdown.text = "ramez !";
+	    	}
+	    	if(compteur <= -0.5){
 	    		countdown.text = "";
 	    	}
-	    	if(!feu){
-	    		compteur-=Time.deltaTime;
-	    		countdown.text = ((int)compteur+1).ToString();
-	    	}
-	   }
-	    else{
-	    	if(FPS.text != "0.0 FPS"){
-	    		//début du compte a rebourd
-	    		pres = true;
-	    		loading.text="Chargement.";
-	    	}
-	    	else{
-	    		//"animation" du chargement
-	    		if(t%3==0){
-	    			loading.text="Chargement.";
-	    		}
-	    		if(t%3==1){
-	    			loading.text="Chargement..";
-	    		}
-	    		if(t%3==2){
-	    			loading.text="Chargement...";
-	    		}
-	    	}
-	   }
-    }
+		}
+	}
+
+	string Temps(float t,string separateur){
+		string minute,seconde,decim;
+		string retour = "";
+		int sec = (int) t;
+		int dec = (int) ((t-sec)*100.0);
+		int min = (int) sec/60;
+
+		minute = min.ToString();
+		seconde = (sec%60).ToString();
+		decim = dec.ToString();
+		if(dec<10)
+			decim = "0"+decim;
+		if(min>0){
+			retour = minute+separateur;
+			if((sec%60)<10)
+				seconde = "0"+seconde;
+		}
+
+		return retour+seconde+separateur+decim;
+	}
+
+	void updateChrono(){
+		if(feu){
+			chronometre += Time.deltaTime;
+    		chrono.text = Temps(chronometre,":");
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		updateCountdown();
+		updateChrono();
+		updateLoading();
+	}
 }
