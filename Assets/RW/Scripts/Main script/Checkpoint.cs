c899f18e154aa6ce17111d37f0eae2646a8c9d40
Jonathan <Sir.Lorrak@gmail.com>
2021-01-11 18:31:36 +0100

    course pour test

--- Assets/RW/Scripts/Main script/Checkpoint.cs
+++ Assets/RW/Scripts/Main script/Checkpoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
 	public CharacterController player;
+	public Material used;
 
 
 	private bool ramasser = false;
@@ -21,13 +22,13 @@ public class Checkpoint : MonoBehaviour
     			boat.RamasserCheckpoint();
     			Debug.Log("vous ramasser un checkpoint");
     			ramasser = true;
+    			gameObject.transform.parent.GetComponent<MeshRenderer>().material = used;
     		}
     		else
     			Debug.Log("vous avez déjà ramasser ce checkpoint");
     	}
     }
 
-    // Update is called once per frame
     void Update()
     {
         
