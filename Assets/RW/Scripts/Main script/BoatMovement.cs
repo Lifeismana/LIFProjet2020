dd61ea37caf59beeaf835c925402d7061c4f9505
Jonathan <Sir.Lorrak@gmail.com>
2021-01-11 15:16:28 +0100

    Arrivée
    reste a faire des checkpoint et a rendre l'arriver plus jolie

--- Assets/RW/Scripts/Main script/BoatMovement.cs
+++ Assets/RW/Scripts/Main script/BoatMovement.cs
@@ -9,79 +9,100 @@ public class BoatMovement : MonoBehaviour
 	public float forward = 10.0f;
 	public float rotateAngle = 20.0f;
 	public float actionTime = 1.0f;
+    public float checkpointARamasser = 0;
+
+    public Collider finish;
 
 	private float currentLeftActionTime=-3f;
 	private float currentRightActionTime=-3f;
 	private CharacterController controller;
 	private bool rameDroiteLever = false;
 	private bool rameGaucheLever = false;
+    public bool pause = true;
 
     // Start is called before the first frame update
     void Start()
     {
       controller = GetComponent<CharacterController>();
+      Physics.IgnoreCollision(controller,finish);
     }
 
+
+    public void Commencer()
+    {
+        pause = false;
+    }
+
+    public void Terminer()
+    {
+        pause = true;
+    }
+
+    public void RamasserCheckpoint()
+    {
+        checkpointARamasser--;
+    }
     // Update is called once per frame
     void Update()
     {
-    	//rame de droite avec openPose
-    	if(GameObject.Find("4_RWrist")){
-    		float Rhigh = GameObject.Find("4_RWrist").transform.position.y;
-    		if(GameObject.Find("3_RElbow")){
-    			float Relbow = GameObject.Find("3_RElbow").transform.position.y;
-    			if(rameDroiteLever){
- 					if(Rhigh<Relbow){
-    					Debug.Log("je tourne a Droite");
- 						currentRightActionTime = Time.time;
-    					rameDroiteLever = false;
- 					}
-    			}
-    			else{
-    				if(Rhigh>Relbow){
-    					rameDroiteLever = true;
-    				}
-    			}
-    		}
-    	}
-    	//rame de gauche avec openPose
-    	if(GameObject.Find("7_LWrist")){
-    		float Lhigh = GameObject.Find("7_LWrist").transform.position.y;
-    		if(GameObject.Find("6_LElbow")){
-    			float Lelbow = GameObject.Find("6_LElbow").transform.position.y;
-    			if(rameGaucheLever){
- 					if(Lhigh<Lelbow){
-    					Debug.Log("je tourne a gauche");
- 						currentLeftActionTime = Time.time;
-    					rameGaucheLever = false;
- 					}
-    			}
-    			else{
-    				if(Lhigh>Lelbow){
-    					rameGaucheLever = true;
-    				}
-    			}
-    		}
-    	}
+    	if(!pause){
+            //rame de droite avec openPose
+        	if(GameObject.Find("4_RWrist")){
+        		float Rhigh = GameObject.Find("4_RWrist").transform.position.y;
+        		if(GameObject.Find("3_RElbow")){
+        			float Relbow = GameObject.Find("3_RElbow").transform.position.y;
+        			if(rameDroiteLever){
+     					if(Rhigh<Relbow){
+     						currentRightActionTime = Time.time;
+        					rameDroiteLever = false;
+     					}
+        			}
+        			else{
+        				if(Rhigh>Relbow){
+        					rameDroiteLever = true;
+        				}
+        			}
+        		}
+        	}
+        	//rame de gauche avec openPose
+        	if(GameObject.Find("7_LWrist")){
+        		float Lhigh = GameObject.Find("7_LWrist").transform.position.y;
+        		if(GameObject.Find("6_LElbow")){
+        			float Lelbow = GameObject.Find("6_LElbow").transform.position.y;
+        			if(rameGaucheLever){
+     					if(Lhigh<Lelbow){
+     						currentLeftActionTime = Time.time;
+        					rameGaucheLever = false;
+     					}
+        			}
+        			else{
+        				if(Lhigh>Lelbow){
+        					rameGaucheLever = true;
+        				}
+        			}
+        		}
+        	}
+        
+            //input de la souris pour les personnes ne possedant pas de caméra 
+            if(Input.GetButton("Fire1") && Time.time - currentLeftActionTime>actionTime){
+            	currentLeftActionTime = Time.time;
+            }
+            if(Input.GetButton("Fire2") && Time.time - currentRightActionTime>actionTime){
+            	currentRightActionTime = Time.time;
+            }
+        }
 
-    	if(Input.GetButton("Fire1") && Time.time - currentLeftActionTime>actionTime){
-    		Debug.Log("je tourne a gauche");
-    		currentLeftActionTime = Time.time;
-    	}
-    	if(Input.GetButton("Fire2") && Time.time - currentRightActionTime>actionTime){
-    		Debug.Log("je tourne a droite");
-    		currentRightActionTime = Time.time;
-    	}
-    	Vector3 MoveVelocity = transform.TransformDirection(Vector3.forward*forward*Time.deltaTime);
-    	if(Time.time - currentLeftActionTime < actionTime){
-    		controller.Move(MoveVelocity);
-    		transform.Rotate(Vector3.up, -rotateAngle*(Time.deltaTime/actionTime));
-        rameGauche.Rotate(Vector3.left, -360*(Time.deltaTime/actionTime));
-    	}
-    	if(Time.time - currentRightActionTime < actionTime){
-    		controller.Move(MoveVelocity);
-    		transform.Rotate(Vector3.up, rotateAngle*(Time.deltaTime/actionTime));
-        rameDroite.Rotate(Vector3.left, -360*(Time.deltaTime/actionTime));
-    	}
+       
+        Vector3 MoveVelocity = transform.TransformDirection(Vector3.forward*forward*Time.deltaTime);
+        if(Time.time - currentLeftActionTime < actionTime){
+        	controller.Move(MoveVelocity);
+        	transform.Rotate(Vector3.up, -rotateAngle*(Time.deltaTime/actionTime));
+            rameGauche.Rotate(Vector3.left, -360*(Time.deltaTime/actionTime));
+        }
+        if(Time.time - currentRightActionTime < actionTime){
+        	controller.Move(MoveVelocity);
+        	transform.Rotate(Vector3.up, rotateAngle*(Time.deltaTime/actionTime));
+            rameDroite.Rotate(Vector3.left, -360*(Time.deltaTime/actionTime));
+        }
     }
 }
---
+++ Assets/RW/Scripts/Main script/FinishLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col){
+    	BoatMovement boat = col.gameObject.GetComponent<BoatMovement>();
+    	if (boat != null){
+    		boat.Terminer();
+    		Debug.Log("Vous êtes arriver bravo");
+    	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
